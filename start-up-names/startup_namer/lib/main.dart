ff3e71a434a9bf81719d26a08c7fc51fcca030a6
David Dickinson <rnddave@gmail.com>
2022-05-19 11:32:44 +0100

    startup generator - moved the random words functionality into its own dart file

--- start-up-names/startup_namer/lib/main.dart
+++ start-up-names/startup_namer/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:english_words/english_words.dart';
+import './random_words.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -28,45 +28,4 @@ class MyApp extends StatelessWidget {
 }
 // #enddocregion MyApp
 
-// #docregion RWS-var
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18);
-  // #enddocregion RWS-var
 
-  // #docregion RWS-build
-  @override
-  Widget build(BuildContext context) {
-    // #docregion itemBuilder
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: /*1*/ (context, i) {
-        if (i.isOdd) return const Divider(); /*2*/
-
-        final index = i ~/ 2; /*3*/
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-        }
-        // #docregion listTile
-        return ListTile(
-          title: Text(
-            _suggestions[index].asPascalCase,
-            style: _biggerFont,
-          ),
-        );
-        // #enddocregion listTile
-      },
-      // #enddocregion itemBuilder
-    );
-  }
-  // #enddocregion RWS-build
-  // #docregion RWS-var
-}
-// #enddocregion RWS-var
-
-class RandomWords extends StatefulWidget {
-  const RandomWords({Key? key}) : super(key: key);
-
-  @override
-  State<RandomWords> createState() => _RandomWordsState();
-}
