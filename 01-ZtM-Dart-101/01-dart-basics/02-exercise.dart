72b4893324c158aca322660801c0f0a2381c2838
David Dickinson <rnddave@gmail.com>
2023-12-14 11:09:41 +0000

    continue to work with variables

---
+++ 01-ZtM-Dart-101/01-dart-basics/02-exercise.dart
@@ -0,0 +1,41 @@
+// more simple variable work
+
+void main() {
+  double temperature = 20;
+  int value = 2;
+  String pizza = 'pizza';
+  String pasta = 'pasta';
+
+/*
+  We want this output:
+
+  The temperature is 20C
+  2 plus 2 makes 4
+  I like pizza and pasta */
+
+  print('The temperature is ${temperature}C');
+  print('$value plus $value makes ${value * value}');
+  print('I like $pizza and $pasta');
+
+  /*
+  OUTPUT: 
+
+  The temperature is 20.0C // THIS IS NOT WHAT WE WANT
+  2 plus 2 makes 4
+  I like pizza and pasta
+  
+  */
+
+  print('The temperature is ${temperature.round()}C');
+  print('$value plus $value makes ${value * value}');
+  print('I like $pizza and $pasta');
+
+  /*
+  OUTPUT: 
+
+  The temperature is 20C 
+  2 plus 2 makes 4
+  I like pizza and pasta
+  
+  */
+}
