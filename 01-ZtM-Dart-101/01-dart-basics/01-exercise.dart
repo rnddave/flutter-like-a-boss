62d10c1292de52e26120e8b2e2182afc38f0044c
David Dickinson <rnddave@gmail.com>
2023-12-14 10:52:29 +0000

    some variable types and string interpolation

---
+++ 01-ZtM-Dart-101/01-dart-basics/01-exercise.dart
@@ -0,0 +1,11 @@
+// print various variables;
+
+void main() {
+  String firstName = 'Andrea';
+  String lastName = 'Bizzotto';
+  int age = 36;
+  double height = 1.84;
+
+  print(
+      'The teacher is called $firstName $lastName, he is $age and stands at $height tall');
+}
--- 01-ZtM-Dart-101/01-dart-basics/dart-test.dart
+++ 01-ZtM-Dart-101/01-dart-basics/dart-test.dart
@@ -1,6 +1,12 @@
 void main() {
+  String name = 'david';
+  int age = 44;
+
   for (int i = 0; i < 5; i++) {
     print(
         'This is my first Dart thingy to check it works in VSCode >> I am counting ${i + 1}');
   }
+  print('My name is ' +
+      name +
+      ' I am $age'); // needed the $ for string interpolation
 }
