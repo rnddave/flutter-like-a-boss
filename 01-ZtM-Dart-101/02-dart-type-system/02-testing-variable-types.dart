7d1d5fc15cee671e89e33ced6917308b813aec4b
David Dickinson <rnddave@gmail.com>
2023-12-19 13:29:47 +0000

    cleaning up some files from the initial directory build out

---
+++ 01-ZtM-Dart-101/02-dart-type-system/02-testing-variable-types.dart
@@ -0,0 +1,7 @@
+void main() {
+  String nameThree = 'OneMoreDavid';
+  print(nameThree.runtimeType);
+
+  var nameFive = 'OneMore_David';
+  print(nameFive.runtimeType);
+}
--- 01-ZtM-Dart-101/03-control-flow/readme.md
+++
@@ -1,1 +0,0 @@
-space-filler - to generate the directory structure
