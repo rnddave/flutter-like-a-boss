333bfa7671d303730df632b34f74cd02eb87ea98
David Dickinson <rnddave@gmail.com>
2024-01-09 10:22:50 +0000

    sets in flutter

--- 01-ZtM-Dart-101/05-collections/05-sets.dart
+++ 01-ZtM-Dart-101/05-collections/05-sets.dart
@@ -10,9 +10,33 @@ void main() {
   // no error - but will not add duplicate
   print(countries); // {France, UK, China, US, Iceland}
 
-  // similar to lists >> 
+  // similar to lists >>
   print(countries.first); //  France
   print(countries.last); // Iceland
   print(countries.length); // 5
+
+  print(countries.contains('UK')); // true
+
+  ///////// DIFFERENCES \\\\\\\\\\\\\\\
+  // These things can be done in SETS but not in LISTS
+
+  var euCountries = {'France', 'UK', 'Germany', 'Russia'};
+  var asianCountries = {'Russia', 'China', 'Japan'};
+
+  // Union (joining)
+  print(euCountries.union(asianCountries));
+  // {France, UK, Germany, Russia, China, Japan}
+  // NB Russia only appears once, a set is a collection of UNIQUE elements remember!! 
+
+  // intersection (what is the same between them)
+  print(euCountries.intersection(asianCountries));
+  // {Russia}
+
+  // difference (what is not the same... (though only against the first set...))
+  print(euCountries.difference(asianCountries));
+  // {France, UK, Germany}
+
+
+
 
 }
