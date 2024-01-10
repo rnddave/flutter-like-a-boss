76dd96b5f233bd78797bba3df815e5d1ae57b48b
David Dickinson <rnddave@gmail.com>
2024-01-10 11:03:32 +0000

    shopping list exercise (using collection-if, collection-for, spreads)

---
+++ 01-ZtM-Dart-101/05-collections/16-shopping-list-exercise.dart
@@ -0,0 +1,48 @@
+/*
+Given the following program:
+
+void main() {
+  const bananas = 5;
+  const apples = 6;
+  const grains = {
+    'pasta': '500g',
+    'rice': '1kg',
+  };
+  const addGrains = true;
+  var shoppingList = {};
+  if (bananas > 0) {
+    shoppingList['bananas'] = bananas;
+  }
+  if (apples > 0) {
+    shoppingList['apples'] = apples;
+  }
+  if (addGrains) {
+    shoppingList.addAll(grains);
+  }
+  print(shoppingList);
+  // prints {bananas: 5, apples: 6, pasta: 500g, rice: 1kg}
+}
+Rewrite this code so that the shoppingList variable is initialized using 
+collection-if, collection-for or spreads, and the program output is the same.
+*/
+
+void main() {
+  const bananas = 5;
+  const apples = 6;
+  const grains = {
+    'pasta': '500g',
+    'rice': '1kg',
+  };
+
+  const addGrains = true;
+
+  final shoppingList = {
+    if (bananas > 0) 'bananas': bananas,
+    if (apples > 0) 'apples': apples,
+    if (addGrains) ...grains,
+  };
+
+  print(shoppingList);
+  // prints {bananas: 5, apples: 6, pasta: 500g, rice: 1kg}
+}
+
