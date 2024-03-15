597293ebc1d902d8139708d5d4f4722ba03d164f
David Dickinson <rnddave@gmail.com>
2024-03-15 11:19:05 +0000

    a super simple navigation showing push and pop at work

---
+++ 02-ZtM-Flutter-101/01-intro/navigation_demo/lib/pages/first.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('First Page'),
+      backgroundColor: Theme.of(context).colorScheme.inversePrimary,),
+    );
+  }
+}
